c83d2e66c865a02c130180673047010c822a1a1f
sumanmadan <sumanvb@gmail.com>
2017-07-03 16:09:55 -0400

    Update a.java

--- a.java
+++ a.java
@@ -1,36 +1,158 @@
-import java.io.File;
-import java.io.IOException;
 
-import javax.swing.JFileChooser;
+ 
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.filechooser.*;
 import javax.swing.filechooser.FileFilter;
-import javax.swing.filechooser.FileNameExtensionFilter;
-
-
-public class a extends utils{
-  public static void main(String[] args) {
-    JFileChooser fileopen = new JFileChooser();
-    FileFilter filter = new FileNameExtensionFilter("dis files", "dis");
-    fileopen.addChoosableFileFilter(filter);
-
-    int ret = fileopen.showDialog(null, "Open file");
-
-    if (ret == JFileChooser.APPROVE_OPTION) {
-      File file = fileopen.getSelectedFile();
-      String path = fileopen.getSelectedFile().getAbsolutePath();
-      String name = fileopen.getSelectedFile().getName();
-      
-      System.out.println(file);
-      System.out.println(path);
-      
-      String dest = System.getProperty("user.home");
-      File destPath = new File(dest +"/Downloads/" + name ); 
-      
-      System.out.println("Copied to :" + destPath);
-      
-      long start = System.nanoTime();
-      //copyFileUsingStream(file, destPath);
-      
-      //System.out.println("Time taken by Stream Copy = "+(System.nanoTime()-start));
+ 
+/*
+ * FileChooserDemo.java uses these files:
+ *   images/Open16.gif
+ *   images/Save16.gif
+ */
+public class a extends JPanel implements ActionListener {
+	
+	
+    static private final String newline = "\n";
+    JButton openButton, saveButton;
+    JTextArea log;
+    JFileChooser fc;
+    utils U = new utils();
+    public a() {
+        super(new BorderLayout());
+ 
+        //Create the log first, because the action listeners
+        //need to refer to it.
+        log = new JTextArea(5,20);
+        log.setMargin(new Insets(5,5,5,5));
+        log.setEditable(false);
+        JScrollPane logScrollPane = new JScrollPane(log);
+ 
+        //Create a file chooser
+        fc = new JFileChooser();
+ 
+        //Uncomment one of the following lines to try a different
+        //file selection mode.  The first allows just directories
+        //to be selected (and, at least in the Java look and feel,
+        //shown).  The second allows both files and directories
+        //to be selected.  If you leave these lines commented out,
+        //then the default mode (FILES_ONLY) will be used.
+        //
+        //fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        //fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+ 
+        //Create the open button.  We use the image from the JLF
+        //Graphics Repository (but we extracted it from the jar).
+        openButton = new JButton("Open a File...", createImageIcon("images/Open16.gif"));
+        openButton.addActionListener(this);
+ 
+        //Create the save button.  We use the image from the JLF
+        //Graphics Repository (but we extracted it from the jar).
+        saveButton = new JButton("Save a File...",
+                                 createImageIcon("images/Save16.gif"));
+        saveButton.addActionListener(this);
+ 
+        //For layout purposes, put the buttons in a separate panel
+        JPanel buttonPanel = new JPanel(); //use FlowLayout
+        buttonPanel.add(openButton);
+        buttonPanel.add(saveButton);
+ 
+        //Add the buttons and the log to this panel.
+        add(buttonPanel, BorderLayout.PAGE_START);
+        add(logScrollPane, BorderLayout.CENTER);
     }
-  }
+ 
+    public void actionPerformed(ActionEvent e) {
+ 
+        //Handle open button action.
+        if (e.getSource() == openButton) {
+        	  
+        	JFileChooser fileopen = new JFileChooser();
+            FileFilter filter = new FileNameExtensionFilter("dis files", "dis");
+            fileopen.addChoosableFileFilter(filter);
+
+            int ret = fileopen.showDialog(null, "Open file");
+
+            if (ret == JFileChooser.APPROVE_OPTION) {
+              File source = fileopen.getSelectedFile();
+              String path = fileopen.getSelectedFile().getAbsolutePath();
+              String name = fileopen.getSelectedFile().getName();
+              
+              System.out.println(source);
+              System.out.println(path);
+              
+              String userHome = System.getProperty("user.home");
+              String destPathStr = userHome + File.separator + "Downloads" + File.separator + "JCALGUI"; 
+              File dest = new File(destPathStr + name); 
+              boolean chk = utils.fileExists(destPathStr);
+              log.append("Folder: "   + destPathStr + " Exists =  "  + chk + newline );
+              long start = System.nanoTime();
+ 
+              //utils.copyFileUsingStream(source, dest);
+			  log.append(source + " Copied to " + "Folder: "   + destPathStr   + newline );
+              
+              //System.out.println("Time taken by Stream Copy = "+(System.nanoTime()-start));
+            }
+            	
+            	
+            	
+            	
+           
+          
+        //Handle save button action.
+        } else if (e.getSource() == saveButton) {
+            int returnVal = fc.showSaveDialog(a.this);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File file = fc.getSelectedFile();
+                //This is where a real application would save the file.
+                log.append("Saving: " + file.getName() + "." + newline);
+            } else {
+                log.append("Save command cancelled by user." + newline);
+            }
+            log.setCaretPosition(log.getDocument().getLength());
+        }
+    }
+ 
+    /** Returns an ImageIcon, or null if the path was invalid. */
+    protected static ImageIcon createImageIcon(String path) {
+        java.net.URL imgURL = a.class.getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+ 
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event dispatch thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("JCALGUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        //Add content to the window.
+        frame.add(new FileChooserDemo());
+ 
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+ 
+    public static void main(String[] args) {
+        //Schedule a job for the event dispatch thread:
+        //creating and showing this application's GUI.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                //Turn off metal's use of bold fonts
+                UIManager.put("swing.boldMetal", Boolean.FALSE); 
+                createAndShowGUI();
+            }
+        });
+    }
 }
