8095116506f2bd3f65dbe2fca02a57a6e323d708
sumanmadan <sumanvb@gmail.com>
2017-07-10 17:16:44 -0400

    Update IndexFileReader.java

--- IndexFileReader.java
+++ IndexFileReader.java
@@ -4,35 +4,67 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.HashMap;
+import java.util.Map;
 
 
 public class IndexFileReader {
 	
 	
 	
-	HashMap<String, String> productList = new HashMap<String, String>();
+	static HashMap<String, String> productList = new HashMap<String, String>();
 	
-	HashMap<String, String> insertionList = new HashMap<String, String>();
+	static HashMap<String, String> insertionList = new HashMap<String, String>();
 	
-	HashMap<String, String> pdList = new HashMap<String, String>();
+	static HashMap<String, String> pdList = new HashMap<String, String>();
 	
-	HashMap<String, String> lotList = new HashMap<String, String>();
+	static HashMap<String, String> lotList = new HashMap<String, String>();
 	
-	HashMap<String, String> waferList = new HashMap<String, String>();
+	static HashMap<String, String> waferList = new HashMap<String, String>();
 	
-	HashMap<String, String> scribeList = new HashMap<String, String>();
+	static HashMap<String, String> scribeList = new HashMap<String, String>();
 	
 	static String filter = "FAB8_";
 	
 	static String splitLine[];
 	
+	static String customerFileInfo = "";
+    static String productIDFileInfo ="";
+    static String lotFileInfo= "";
+    static String PDFileInfo = "";
+    static String InsertionFileInfo = "";
+    static String WaferFileInfo = "";
+    static String ScribeFileInfo = "";
+    static String DateTimeFileInfo = "";
+    
+    
+    public IndexFileReader() throws IOException {
+    	
+
+    	readFile();
+		printProductList();
+    }
+	
 	public static void main (String args[]) throws IOException {
 		
-		readFile();
+	
+		//readFile();
+		//printProductList();
+	
+		
 	}
 		
-	public static void readFile() throws IOException{
+	public static HashMap printProductList() {
+		// TODO Auto-generated method stub
+		for (Map.Entry<String, String> entry : productList.entrySet()) {
+		    System.out.println("................. + ........ " + entry.getKey() +" : "+entry.getValue());
+		    
+		}
 		
+		return productList;
+	}
+
+	public static void readFile() throws IOException {
+		utils u = new utils();
 		BufferedReader br = new BufferedReader(new FileReader("\\\\fc8fstp01\\wet_public\\INDEX\\2017\\19.index"));
 		/*
 		amd/ZEPPLN14MJ.H0/M73N5.00/LV-WETILTH-14LPP.01/FWET                | 
@@ -42,18 +74,41 @@ public class IndexFileReader {
 		
 	
 		*/
-		
+		 
 		  String line;
 		  int i = 0;
 
 		  while ((line = br.readLine()) != null) {
 			  
-			  System.out.println("***********"  + line);
+			 // System.out.println("***********"  + line);
 			  
 			  if ( line.contains(filter)) {
-			  splitLine = line.split("|");
-			  
-			  if ( splitLine[1].startsWith(filter)) {
+				  
+				
+			      splitLine = line.split("\\|");
+			     
+			  if ( splitLine[1].trim().startsWith(filter)) {
+				  
+				u.splitFileInfo(splitLine[1]);
+				
+				productIDFileInfo = u.getProductIDFileInfo();
+				System.out.println("********ccc***"  + u.getProductIDFileInfo());
+				lotFileInfo = utils.getlotFileInfo();
+				PDFileInfo = utils.getPDFileInfo();
+				InsertionFileInfo = utils.getInsertionFileInfo();
+				WaferFileInfo = utils.getWaferFileInfo();
+				
+				if ( productList.containsKey(productIDFileInfo)) {
+					
+					productList.remove(productIDFileInfo);
+					productList.put(productIDFileInfo, productIDFileInfo);
+				
+					
+			
+				} else {
+					productList.put(productIDFileInfo, productIDFileInfo);
+					
+				}
 				  
 			  }
 			  
